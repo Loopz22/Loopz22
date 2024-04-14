a3e4af171427c8247e4939851ec51b21c6a04a6a
Loopz <147947278+Loopz22@users.noreply.github.com>
2024-04-14 18:50:41 -0300

    Create animations.js

---
+++ animations.js
@@ -0,0 +1,5 @@
+gsap.from('.about-img', { duration: 1, x: -200, opacity: 0, ease: 'power4.out', delay: 0.5 });
+gsap.from('.skills-icons img', { duration: 1, y: 200, opacity: 0, ease: 'power4.out', stagger: 0.2, delay: 0.5 });
+gsap.from('.content', { duration: 1, y: 200, opacity: 0, ease: 'power4.out', stagger: 0.2, delay: 0.5 });
+gsap.from('.github-stats', { duration: 1, y: 200, opacity: 0, ease: 'power4.out', delay: 0.5 });
+gsap.from('.github-languages', { duration: 1, y: 200, opacity: 0, ease: 'power4.out', delay: 0.5 });
